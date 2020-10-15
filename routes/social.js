a3a63d3c94d35600adf670e7609ef60e852c87f4
권수진 <kwonsujin@gwonsujin-ui-MacBookAir.local>
2020-10-15 16:37:33 +0900

    kakaoSocial_ing

--- routes/social.js
+++ routes/social.js
@@ -1,9 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const socialController = require("../controller/social");
+const socialController = require('../controller/social');
 
 //판매내역을 가져오는 get요청은 userinfo/mymenu api를 이용한다
-router.get("/kakao", socialController.kakao.get);
-
+router.get('/kakao', socialController.kakaoSignup.get);
+router.get('/registered', socialController.registered.get);
+router.get('/existing', socialController.existing.get);
+router.get('/unlink', socialController.unLink.get);
 module.exports = router;
